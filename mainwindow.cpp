64c504d0a8c09436ad97abfafddc04d224f6434a
Flagg <flaggyellow@qq.com>
2020-12-08 17:46:32 +0800

    select file path

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,24 +2,58 @@
 #include "ui_mainwindow.h"
 
 #include <QFileDialog>
+#include <QLineEdit>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    // ui->input_source->text();
-    // connect(ui->convert_source_select_button, SIGNAL(clicked()), this, SLOT(send_text()));
-}
 
+    // connects
+    connect(ui->convert_source_select_button, SIGNAL(clicked()), this, SLOT(fetch_file_path_convert_source()));
+    connect(ui->convert_target_select_button, SIGNAL(clicked()), this, SLOT(fetch_file_path_convert_target()));
+    connect(ui->recover_source_select_button, SIGNAL(clicked()), this, SLOT(fetch_file_path_recover_source()));
+    connect(ui->recover_target_select_button, SIGNAL(clicked()), this, SLOT(fetch_file_path_recover_target()));
+}
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
-//void MainWindow::send_text(){
-//    ui->convert_input_source->setText("Hello World!");
-//    QFileDialog *win1 = new QFileDialog(this);
-//    win1->show();
-//}
-
+void MainWindow::fetch_file_path_convert_source()
+{
+    ui->convert_input_source->setText(fetch_file_path(0));
+}
+void MainWindow::fetch_file_path_convert_target()
+{
+    ui->convert_input_target->setText(fetch_file_path(0));
+}
+void MainWindow::fetch_file_path_recover_source()
+{
+    ui->recover_input_source->setText(fetch_file_path(1));
+}
+void MainWindow::fetch_file_path_recover_target()
+{
+    ui->recover_input_target->setText(fetch_file_path(0));
+}
+QString MainWindow::fetch_file_path(int mode)   //mode 0:directory only / 1:file only
+{
+    QFileDialog *fileDialog = new QFileDialog(this);
+    fileDialog->setWindowTitle(QString::fromLocal8Bit("选择文件路径..."));
+    if(mode == 0)
+    {
+        fileDialog->setFileMode(QFileDialog::Directory);
+    }else{
+        fileDialog->setFileMode(QFileDialog::ExistingFiles);
+    }
+    fileDialog->setDirectory("C:/");
+    fileDialog->setViewMode(QFileDialog::Detail);
+    QStringList filePath;
+    if (fileDialog->exec())
+    {
+        filePath = fileDialog->selectedFiles();
+    }
+    if (filePath.length() > 0) return filePath[0];
+    else return "";
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLineEdit>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,10 +16,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-//private slots:
-//    void send_text();
+public slots:
+    void fetch_file_path_convert_source();
+    void fetch_file_path_convert_target();
+    void fetch_file_path_recover_source();
+    void fetch_file_path_recover_target();
 
 private:
     Ui::MainWindow *ui;
+    QString fetch_file_path(int mode);
 };
 #endif // MAINWINDOW_H
